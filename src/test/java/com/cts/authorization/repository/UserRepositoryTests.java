1b3816f541d4c97b580bce8a77277dac9816333b
SGhosh-007 <gsusho123@gmail.com>
2022-05-30 02:06:19 +0530

    final commit

--- src/test/java/com/cts/authorization/repository/UserRepositoryTests.java
+++ src/test/java/com/cts/authorization/repository/UserRepositoryTests.java
@@ -5,13 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.util.StringUtils;
 
 import com.cts.authorization.model.User;
 
@@ -48,7 +48,7 @@ class UserRepositoryTests {
 		log.info("START - testFindUserById_userDoesNotExists()");
 		final String id = "adminDoesNotExist";
 		Optional<User> userOptional = userRepository.findById(id);
-		assertTrue(userOptional.isEmpty());
+		assertTrue(StringUtils.isEmpty(userOptional));
 		log.info("END - testFindUserById_userDoesNotExists()");
 	}
 }
